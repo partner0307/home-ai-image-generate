03b6ffc23228ec52195785ae21983096f529eb79
ItamiWorld <iris.minami0307@gmail.com>
2023-06-29 03:06:32 +0900

    update: footer

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, P, Link } from "components/basic";
 import { FooterContainer } from "./style"
 import { Icon } from "components/custom";
+import { GV } from "utils/style.util";
 
 
 const Footer = () => {
@@ -8,13 +9,15 @@ const Footer = () => {
         <FooterContainer>
             <P>Copyright 2023 rendro. All Rights Reserved</P>
             <Flex $style={{
-                fDirection: 'column',
-                gap: "0.25rem"
+                fDirection: 'row',
+                gap: '1rem'
             }}>
-                <P>Rendro@info.ca</P>
+                <P $style={{
+                    color: GV('primary')
+                }}>Rendro@info.ca</P>
                 <Flex $style={{
                     fDirection: 'row',
-                    gap: '0.75rem'
+                    gap: '0.75rem',
                 }}>
                     <Icon icon="Youtube" />
                     <Icon icon="Discord" />
